89939cb428bd36a7cf0e2776478d7db2d8cc1ef0
Patrick Podest <patrick@podest.co.at>
2019-03-13 08:35:58 +0100

    Add/Remove blank lines in FeaturePython.py
    This was done to make the formatting of the whole file more
    consistent in itself.

--- src/Mod/TemplatePyMod/FeaturePython.py
+++ src/Mod/TemplatePyMod/FeaturePython.py
@@ -155,7 +155,7 @@ class Octahedron:
 		obj.addProperty("App::PropertyLength","Height","Octahedron", "Height of the octahedron").Height=1.0
 		obj.addProperty("Part::PropertyPartShape","Shape","Octahedron", "Shape of the octahedron")
 		obj.Proxy = self
- 
+
 	def execute(self, fp):
 		# Define six vetices for the shape
 		v1 = FreeCAD.Vector(0,0,0)
@@ -164,7 +164,7 @@ class Octahedron:
 		v4 = FreeCAD.Vector(fp.Length,fp.Width,0)
 		v5 = FreeCAD.Vector(fp.Length/2,fp.Width/2,fp.Height/2)
 		v6 = FreeCAD.Vector(fp.Length/2,fp.Width/2,-fp.Height/2)
-		
+
 		# Make the wires/faces
 		f1 = self.make_face(v2,v1,v5)
 		f2 = self.make_face(v4,v2,v5)
@@ -177,6 +177,7 @@ class Octahedron:
 		shell=Part.makeShell([f1,f2,f3,f4,f5,f6,f7,f8])
 		solid=Part.makeSolid(shell)
 		fp.Shape = solid
+
 	# helper method to create the faces
 	def make_face(self,v1,v2,v3):
 		wire = Part.makePolygon([v1,v2,v3,v1])
@@ -188,7 +189,7 @@ class ViewProviderOctahedron:
 		"Set this object to the proxy object of the actual view provider"
 		obj.addProperty("App::PropertyColor","Color","Octahedron","Color of the octahedron").Color=(1.0,0.0,0.0)
 		obj.Proxy = self
- 
+
 	def attach(self, obj):
 		"Setup the scene sub-graph of the view provider, this method is mandatory"
 		self.shaded = coin.SoGroup()
@@ -197,7 +198,7 @@ class ViewProviderOctahedron:
 
 		self.data=coin.SoCoordinate3()
 		self.face=coin.SoIndexedFaceSet()
- 
+
 		self.shaded.addChild(self.color)
 		self.shaded.addChild(self.data)
 		self.shaded.addChild(self.face)
@@ -210,7 +211,7 @@ class ViewProviderOctahedron:
 		self.wireframe.addChild(self.face)
 		obj.addDisplayMode(self.wireframe,"Wireframe");
 		self.onChanged(obj,"Color")
- 
+
 	def updateData(self, fp, prop):
 		"If a property of the handled feature has changed we have the chance to handle this here"
 		# fp is the handled feature, prop is the name of the property that has changed
@@ -221,7 +222,7 @@ class ViewProviderOctahedron:
 			for i in s.Vertexes:
 				self.data.point.set1Value(cnt,i.X,i.Y,i.Z)
 				cnt=cnt+1
-			
+
 			self.face.coordIndex.set1Value(0,0)
 			self.face.coordIndex.set1Value(1,2)
 			self.face.coordIndex.set1Value(2,1)
@@ -231,32 +232,32 @@ class ViewProviderOctahedron:
 			self.face.coordIndex.set1Value(5,2)
 			self.face.coordIndex.set1Value(6,0)
 			self.face.coordIndex.set1Value(7,-1)
- 
+
 			self.face.coordIndex.set1Value(8,4)
 			self.face.coordIndex.set1Value(9,2)
 			self.face.coordIndex.set1Value(10,3)
 			self.face.coordIndex.set1Value(11,-1)
- 
+
 			self.face.coordIndex.set1Value(12,1)
 			self.face.coordIndex.set1Value(13,2)
 			self.face.coordIndex.set1Value(14,4)
 			self.face.coordIndex.set1Value(15,-1)
- 
+
 			self.face.coordIndex.set1Value(16,1)
 			self.face.coordIndex.set1Value(17,5)
 			self.face.coordIndex.set1Value(18,0)
 			self.face.coordIndex.set1Value(19,-1)
- 
+
 			self.face.coordIndex.set1Value(20,0)
 			self.face.coordIndex.set1Value(21,5)
 			self.face.coordIndex.set1Value(22,3)
 			self.face.coordIndex.set1Value(23,-1)
- 
+
 			self.face.coordIndex.set1Value(24,3)
 			self.face.coordIndex.set1Value(25,5)
 			self.face.coordIndex.set1Value(26,4)
 			self.face.coordIndex.set1Value(27,-1)
- 
+
 			self.face.coordIndex.set1Value(28,4)
 			self.face.coordIndex.set1Value(29,5)
 			self.face.coordIndex.set1Value(30,1)
@@ -268,11 +269,11 @@ class ViewProviderOctahedron:
 		modes.append("Shaded")
 		modes.append("Wireframe")
 		return modes
- 
+
 	def getDefaultDisplayMode(self):
 		"Return the name of the default display mode. It must be defined in getDisplayModes."
 		return "Shaded"
- 
+
 	def setDisplayMode(self,mode):
 		return mode
 
@@ -311,10 +312,10 @@ class ViewProviderOctahedron:
 			"  ##$$$$$#      ",
 			"   #######      "};
 			"""
- 
+
 	def __getstate__(self):
 		return None
- 
+
 	def __setstate__(self,state):
 		return None
 
@@ -429,7 +430,6 @@ def makePoints():
 
 	p.addPoints(l)
 
-
 	a=FreeCAD.ActiveDocument.addObject("Points::FeaturePython","Points")
 	a.Points=p
 	PointFeature(a)
@@ -516,7 +516,7 @@ def makeMesh():
 	a.Mesh=Mesh.createSphere(5.0)
 	MeshFeature(a)
 	ViewProviderMesh(a.ViewObject)
-	
+
 # -----------------------------------------------------------------------------
 
 class Molecule:
@@ -524,7 +524,6 @@ class Molecule:
 		''' Add two point properties '''
 		obj.addProperty("App::PropertyVector","p1","Line","Start point")
 		obj.addProperty("App::PropertyVector","p2","Line","End point").p2=FreeCAD.Vector(1,0,0)
-
 		obj.Proxy = self
 
 	def onChanged(self, fp, prop):
@@ -563,7 +562,7 @@ class ViewProviderMolecule:
 
 	def __getstate__(self):
 		return None
- 
+
 	def __setstate__(self,state):
 		return None
 
@@ -606,13 +605,13 @@ class ViewProviderCircleSet:
 					for j in range(10):
 						v=i.valueAt(j/9.0*length)
 						pts.append((v.x,v.y,v.z))
-				
+
 				self.coords.point.setValues(pts)
 				self.lines.numVertices.setValues(ver)
 
 	def __getstate__(self):
 		return None
- 
+
 	def __setstate__(self,state):
 		return None
 
@@ -644,11 +643,11 @@ class EnumTest:
 		obj.Proxy = self
 
 	def execute(self, fp):
-		return                                                                            
-                                                                               
+		return
+
 class ViewProviderEnumTest:
 	def __init__(self, obj):
-		''' Set this object to the proxy object of the actual view provider '''           
+		''' Set this object to the proxy object of the actual view provider '''
 		obj.addProperty("App::PropertyEnumeration","Enum3","","Enumeration3").Enum3=["One","Two","Three"]
 		obj.addProperty("App::PropertyEnumeration","Enum4","","Enumeration4").Enum4=["One","Two","Three"]
 		obj.Proxy = self
